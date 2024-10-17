664bc507524015858aedb418acc52d17e26d77b8
Jean Lucas <j3anlucas010@gmail.com>
2024-10-17 09:50:03 -0300

    Função para calcular o valor total do pedido.

--- script.js
+++ script.js
@@ -7,25 +7,54 @@
 // entregue: valor booleano (inicialmente false), indicando se o pedido foi entregue ou não.
 // Função para adicionar um novo pedido. A função deve receber o nome do cliente e uma lista de itens (com nome e preço), criar um novo pedido e adicioná-lo ao array de pedidos.
 
+// Função para calcular o valor total do pedido. Dada a lista de itens, a função deve somar os preços dos itens para calcular o valor total do pedido.
+
 const pedidos = [
   {
-    id: 1,
-    nome:' ',
-    clientes: '',
-    Entregue: true
+    id: 0,
+    clientes: "",
+    itens: [
+      { nome: "Hambúrguer", preco: 15 },
+      { nome: "Refrigerante", preco: 59 },
+    ],
+    preparado: false,
+    entregue: true,
   },
 ];
 
-function addpedido(cliente,nomes,preco){
-const novopedido=
-        {
-                id:pedidos.length+1,
-                cliente:cliente,
-                itens:nomes, preco
+function addpedido(cliente, itens) {
+  const novopedido = {
+    id: pedidos.length,
+    cliente: cliente,
+    itens: itens,
+    preparado: false,
+    entregue: false,
+  };
 
-        }
+  pedidos.push(novopedido);
+  console.log(
+    `Pedido ${pedidos.length} Para o cliente ${cliente} adicionado com sucesso!`
+  );
 
-pedidos.push(novopedido)
+  return novopedido;
 }
-addpedido('Jean','Blusa',33)
-console.log(pedidos)
+
+function soma(pedido) {
+  const total = pedido.itens.reduce(
+    (acumulador, item) => acumulador + item.preco,
+    0
+  );
+  return total;
+}
+
+addpedido("Jean", [
+  { nome: "Refrigerante", preco: 4.5 },
+  { nome: "Batata Frita", preco: 20 },
+]);
+addpedido("Lucas", [
+  { nome: "Hamburgue", preco: 34.88 },
+  { nome: "Água", preco: 2.5 },
+]);
+const totalPedido = soma(pedidos[2]);
+console.log(pedidos[2])
+console.log(`O valor total do pedido é: R$ ${totalPedido.toFixed(2)}`);
